82d14bbee00f0a3322d2e037cc3c55e2250dd77e
Aajdini <elv-artrit.ajdn@eduge.ch>
2017-05-04 23:39:02 +0200

    Ajout de fonctions dans la classe GildedRoseNew

--- Java/src/main/java/com/gildedrose/GlidedRoseNew.java
+++ Java/src/main/java/com/gildedrose/GlidedRoseNew.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.gildedrose;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author Artrit
  */
 public class GlidedRoseNew {
+    ArrayList listeItem = new ArrayList();
+    
+    public GlidedRoseNew(ArrayList liste) {
+        this.listeItem = liste;
+    }
+    
+    public void updateQuality(Item item) {
+        if(item.isIsIncrease()){increaseQuality(item);}
+        else{decreaseQuality(item);}
+        updateSellIn(item);
+    }
+    
+    private void updateSellIn(Item item){
+        if(item.getSellIn() <= 0){return;}
+        else{item.setSellIn(item.getSellIn()-1);}
+    }
+    
+    private void increaseQuality(Item item){
+        if(item.getSellIn() <= 0){item.setQuality(0);}
+        else if(item.getSellIn() <= 5){item.setQuality(item.getQuality() + 3);}
+        else if(item.getSellIn() <= 10){item.setQuality(item.getQuality() + 2);}
+        else{return;}
+    }
+    private void decreaseQuality(Item item){
+        if(item.getSellIn() == -1){return;}
+        else if(item.getSellIn() <= 0){item.setQuality(item.getQuality() - 2);}
+        else{item.setQuality(item.getQuality() - 1);}
+    }
     
 }
--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -2,16 +2,34 @@ package com.gildedrose;
 
 public class Item {
 
-    public String name;
-
-    public int sellIn;
-
-    public int quality;
-
-    public Item(String name, int sellIn, int quality) {
+    private String name;
+    private int sellIn;
+    private int quality;
+    private boolean isLegendary;
+    private boolean isIncrease;
+    
+    public Item(String name, int sellIn, int quality, boolean isLegendary, boolean isIncrease) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
+        this.isLegendary = isLegendary;
+        this.isIncrease = isIncrease;
+    }
+
+    public void setIsIncrease(boolean isIncrease) {
+        this.isIncrease = isIncrease;
+    }
+
+    public boolean isIsIncrease() {
+        return isIncrease;
+    }
+
+    public boolean isIsLegendary() {
+        return isLegendary;
+    }
+
+    public void setIsLegendary(boolean isLegendary) {
+        this.isLegendary = isLegendary;
     }
 
     public String getName() {
@@ -35,12 +53,9 @@ public class Item {
 
     @Override
     public boolean equals(Object obj) {
-        return super.equals(obj); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public int compareTo(String anotherString) {
-        return name.compareTo(anotherString);
-    }
+        Item item = (Item)obj;
+        return this.name.equals(item.getName());
+    }//equals
    
    @Override
    public String toString() {
