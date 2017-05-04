9a311b4078bfa5e1f7b00ce4c9a740db0a4257d4
Aajdini <elv-artrit.ajdn@eduge.ch>
2017-05-04 17:56:37 +0200

    Ajout de méthodes dans la classe Item (getters/setters/equals) + ajout méthodes dans la classe GlidedRoseNew

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -14,6 +14,34 @@ public class Item {
         this.quality = quality;
     }
 
+    public String getName() {
+        return name;
+    }
+    public int getSellIn() {
+        return sellIn;
+    }
+    public int getQuality() {
+        return quality;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int compareTo(String anotherString) {
+        return name.compareTo(anotherString);
+    }
+   
    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
